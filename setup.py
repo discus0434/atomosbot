5fce35e17c3724550ff26c4ae6dbc20af46ffbf0
Kohei_Ohno <discus0434@gmail.com>
2022-06-26 22:40:04 +0900

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,5 @@ setup(
     name="atomosbot",
     version="1.0.0",
     description="LINEBotで気圧変動や天気・気温を表示する",
-    author="Kohei Ohno",
-    author_email="discus0434@gmail.com",
     url="https://github.com/discus0434/atomosbot",
 )
