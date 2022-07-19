939c413cf7a3083d8181c29731e750dcb76c3a67
Kohei_Ohno <discus0434@gmail.com>
2022-07-19 19:36:53 +0900

    delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="atomosbot",
-    version="1.0.0",
-    description="LINEBotで気圧変動や天気・気温を表示する",
-    url="https://github.com/discus0434/atomosbot",
-)
